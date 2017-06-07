f8c2d5b693be523ff7a1fbebe78e8dc814617d2c
wentianen <wentianen@xingshulin.com>
2017-06-07 08:02:32 +0800

    Initial Commit

---
+++ WalletTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WalletTest
+//
+//  Created by 温天恩 on 2017/6/7.
+//  Copyright © 2017年 温天恩. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
